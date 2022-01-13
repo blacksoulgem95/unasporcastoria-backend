f170e471be6ae8f435f2ba19fa18c8114c30e32a
blacksoulgem95 <11537307+blacksoulgem95@users.noreply.github.com>
2022-01-13 19:12:28 +0100

    cors

---
+++ src/main/java/com/unasporcastoria/core/api/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.unasporcastoria.core.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class WebConfig extends WebSecurityConfigurerAdapter {
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.cors();
+  }
+}
