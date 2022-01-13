658b163b1e258784b9c36f4c2701bec5a5154cb2
blacksoulgem95 <11537307+blacksoulgem95@users.noreply.github.com>
2022-01-13 16:56:01 +0100

    service account

--- src/main/java/com/unasporcastoria/core/api/config/FirebaseConfig.java
+++ src/main/java/com/unasporcastoria/core/api/config/FirebaseConfig.java
@@ -8,19 +8,24 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
 import com.google.firebase.cloud.StorageClient;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 @Configuration
 public class FirebaseConfig {
+  @Value("${cloud.firebase.serviceaccount}")
+  private String serviceAccount;
 
   @Bean
   public FirebaseOptions options() throws IOException {
     return FirebaseOptions.builder()
-        .setCredentials(GoogleCredentials.getApplicationDefault())
+        .setCredentials(GoogleCredentials.fromStream(new ByteArrayInputStream(serviceAccount.getBytes(StandardCharsets.UTF_8))))
         .build();
   }
 
