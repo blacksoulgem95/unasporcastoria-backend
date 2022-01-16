43603dfe6f1994bb7d0d647a1251d7053a256b1f
blacksoulgem95 <11537307+blacksoulgem95@users.noreply.github.com>
2022-01-16 11:33:52 +0100

    fix unit tests to match new item creation format

--- src/test/java/com/unasporcastoria/core/api/dto/ItemCreateDtoTest.java
+++ src/test/java/com/unasporcastoria/core/api/dto/ItemCreateDtoTest.java
@@ -4,48 +4,48 @@ import com.unasporcastoria.core.api.enums.ItemMacroType;
 import com.unasporcastoria.core.api.enums.ItemType;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class ItemCreateDtoTest {
 
-    @Test
-    void toItem() {
-        var dto = getFullDto();
-        var item = dto.toItem();
+  @Test
+  void toItem() {
+    var dto = getFullDto();
+    var item = dto.toItem();
 
-        assertEquals(dto.getName(), item.getName());
-        assertEquals(dto.getDescription(), item.getDescription());
-        assertEquals(dto.getPrice(), item.getPrice());
-        assertEquals(dto.getSellingPrice(), item.getSellingPrice());
-        assertEquals(dto.getValue(), item.getValue());
-        assertEquals(dto.getDots(), item.getDots());
-        assertEquals(dto.getMax(), item.getMax());
-        assertEquals(dto.getBonus(), item.getBonus());
-        assertEquals(dto.getStructPoint(), item.getStructPoint());
-        assertEquals(dto.getType(), item.getType());
-        assertEquals(dto.getMacroType(), item.getMacroType());
-        assertEquals(dto.isEnabled(), item.isEnabled());
-        assertEquals(dto.isConsumable(), item.isConsumable());
-        assertEquals(dto.isOnlyInCreation(), item.isOnlyInCreation());
-    }
+    assertEquals(dto.getName(), item.getName());
+    assertEquals(dto.getDescription(), item.getDescription());
+    assertEquals(dto.getPrice(), (double) item.getPrice() / 10);
+    assertEquals(dto.getSellingPrice(), (double) item.getSellingPrice() / 10);
+    assertEquals(dto.getValue(), (double) item.getValue() / 10);
+    assertEquals(dto.getDots(), item.getDots());
+    assertEquals(dto.getMax(), item.getMax());
+    assertEquals(dto.getBonus(), item.getBonus());
+    assertEquals(dto.getStructPoint(), item.getStructPoint());
+    assertEquals(dto.getType(), item.getType());
+    assertEquals(dto.getMacroType(), item.getMacroType());
+    assertEquals(dto.isEnabled(), item.isEnabled());
+    assertEquals(dto.isConsumable(), item.isConsumable());
+    assertEquals(dto.isOnlyInCreation(), item.isOnlyInCreation());
+  }
 
-    private ItemCreateDto getFullDto() {
-        return new ItemCreateDto(
-                "Ajeje",
-                "Brazorf",
-                1L,
-                2L,
-                3L,
-                4L,
-                5L,
-                6L,
-                7L,
-                ItemType.TYPE_ARMOR_SHIELD,
-                ItemMacroType.BUILDING,
-                true,
-                false,
-                true
-        );
-    }
+  private ItemCreateDto getFullDto() {
+    return new ItemCreateDto(
+        "Ajeje",
+        "Brazorf",
+        0.1,
+        0.2,
+        0.3,
+        4L,
+        5L,
+        6L,
+        7L,
+        ItemType.TYPE_ARMOR_SHIELD,
+        ItemMacroType.BUILDING,
+        true,
+        false,
+        true
+    );
+  }
 }
