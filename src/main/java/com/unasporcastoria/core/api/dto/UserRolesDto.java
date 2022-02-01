48e8dddb107392747b5bd2a4c76730c177e27f6e
blacksoulgem95 <11537307+blacksoulgem95@users.noreply.github.com>
2022-02-01 21:30:07 +0100

    Could not deserialize object. Collections are not supported, please use Lists instead (found in field 'roles')

--- src/main/java/com/unasporcastoria/core/api/dto/UserRolesDto.java
+++ src/main/java/com/unasporcastoria/core/api/dto/UserRolesDto.java
@@ -4,11 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
 import java.util.Set;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserRolesDto {
-  private Set<String> roles = Set.of();
+  private List<String> roles = List.of();
 }
