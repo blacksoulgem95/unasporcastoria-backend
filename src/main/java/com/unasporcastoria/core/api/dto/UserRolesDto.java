474707573c485ab901ca13059d8045a573657dd4
blacksoulgem95 <11537307+blacksoulgem95@users.noreply.github.com>
2022-02-01 21:25:57 +0100

    serialization

--- src/main/java/com/unasporcastoria/core/api/dto/UserRolesDto.java
+++ src/main/java/com/unasporcastoria/core/api/dto/UserRolesDto.java
@@ -2,11 +2,13 @@ package com.unasporcastoria.core.api.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class UserRolesDto {
-  private Set<String> roles;
+  private Set<String> roles = Set.of();
 }
